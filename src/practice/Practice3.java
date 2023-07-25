ab5384cc23c536344942baafabc1c1123b44e894
cchoijjinyoung <cgy7127@naver.com>
2023-07-25 14:29:32 +0900

    star

--- src/practice/Practice3.java
+++ src/practice/Practice3.java
@@ -2,10 +2,53 @@ package practice;
 
 public class Practice3 {
     public static String solution(char[] str, char[] from, char[] to) {
-        int idx = 0;
-        String replaceStr = "";
+        int idx = 0; // 인덱스 변수
+        String replaceStr = ""; // 최종 변경될 문자열
         char[] replaceBucket = str.clone();
-        
+
+        do {
+            idx = findIndex(replaceBucket, from);
+
+            if (idx != -1) {
+                for (int i = 0; i < idx; i++) {
+                    replaceStr += replaceBucket[i];
+                }
+                for (int i = 0; i < to.length; i++) {
+                    replaceStr += to[i];
+                }
+                for (int i = idx + from.length; i < replaceBucket.length; i++) {
+                    replaceStr += replaceBucket[i];
+                }
+
+                replaceBucket = replaceStr.toCharArray();
+                replaceStr = "";
+            }
+
+        } while (idx != -1);
+        replaceStr = new String(replaceBucket);
+        return replaceStr;
+    }
+
+    public static int findIndex(char[] str, char[] from) {
+        int idx = -1;
+        boolean isMatch = false;
+
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] == from[0] && str.length - i >= from.length) {
+                isMatch = true;
+                for (int j = 1; j < from.length; j++) {
+                    if (str[i + j] != from[j]) {
+                        isMatch = false;
+                        break;
+                    }
+                }
+            }
+            if (isMatch) {
+                idx = i;
+                break;
+            }
+        }
+        return idx;
     }
 
     public static void main(String[] args) {
