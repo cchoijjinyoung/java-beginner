3415b91b2f5cef970654a4f4be150268959df5d2
cchoijjinyoung <cgy7127@naver.com>
2023-07-26 10:08:15 +0900

    star type5

--- src/practice/Practice4.java
+++ src/practice/Practice4.java
@@ -67,6 +67,15 @@ public class Practice4 {
 
     public static void type4(int n) {
         System.out.println("== Type4 ==");
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n - i - 1; j++) {
+//                System.out.print(" ");
+//            }
+//            for (int j = 0; j < i * 2 + 1; j++) {
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - i - 1; j++) {
                 System.out.print(" ");
@@ -94,7 +103,13 @@ public class Practice4 {
 
         // 하단부
         for (int i = n / 2; i > 0; i--) {
-            
+            for (int j = 0; j < n / 2 + 1 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i * 2 - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
         System.out.println();
     }
